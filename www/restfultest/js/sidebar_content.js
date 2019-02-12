d12876fe4d079f77c53619574d59adf8bdd81bc5
Brent Clancy <eternialogic@gmail.com>
2019-02-12 17:06:49 -0600

    Update sidebar_content.js

--- www/restfultest/js/sidebar_content.js
+++ www/restfultest/js/sidebar_content.js
@@ -1,5 +1,5 @@
 // Main Javascript file
-// © Brent Clancy 2018
+// Â© Brent Clancy 2018
 // Sidebar content drawing and networking intermediates
 // Author: Brent Clancy (3/1/2016)
 
@@ -30,9 +30,7 @@ function sbGetAbout(){
 	about += '<a href="http://www.instagram.com/" target="_BLANK"><span class="fa fa-instagram">&nbsp;&nbsp;</span></a>&nbsp;';
 	about += '<a href="http://www.twitter.com/" target="_BLANK"><span class="fa fa-twitter">&nbsp;&nbsp;</span></a>&nbsp;';
 	about += '<a href="http://www.facebook.com/" target="_BLANK"><span class="fa fa-facebook">&nbsp;&nbsp;</span></a>';
-	about += '<br />test App<br />© Brent Clancy 2016 (Pending transfer)';
+	about += '<br />test App<br />Â© Brent Clancy 2019';
 	
-	// Pending transfer to, but not applicable until delivered:
-	//about += '<br />test App<br />© Liveride 2016';
 	return about;
 }
